7a4eadbe5324e43259d06d4f3b9ded639b6fc76d
CentipedeZ <dolinekvit@gmail.com>
2020-02-16 12:42:43 +0100

    Pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,7 +31,6 @@ export function HttpLoaderFactory(httpClient: HttpClient) {
     HeaderModule,
     SearchModule,
     HomeModule,
-    NumberFilterPipe,
     FontAwesomeModule,
     TranslateModule.forRoot({
       loader: {
